71e66b4dc714f0815a6abc9a372688ea6100b2b5
theseeduse <qwert304411@naver.com>
2025-01-24 10:21:26 +0900

    파일 require 관련 수정

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const NamumarkSyntax = require('./syntax/index.js');
 let data = `[[분류:뉴시드위키]]\n[목차]\n== 개요 ==\n뉴시드위키는 망했다\nNew seed도 망했다`; // 대충 db 생성하는 코드로 대체
-// const config = require('/config/ClientConfig');
+// const config = require('../../config/ClientConfig');
 
 const document_lmt = 100;
 
